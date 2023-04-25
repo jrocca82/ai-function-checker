327eacaaef5c59afe61573cddcccc8e99f77e632
Jo <loluvulol@gmail.com>
2023-04-25 15:20:31 +1000

    fix: metadata

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -4,13 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Function Translator" key="title"/>
+        <meta property="og:description" content="Translate functions using AI" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="FT.png"
         />
-        <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
         <Main />
